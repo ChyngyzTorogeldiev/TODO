b32364816039dab19ef3ec223b8677b51f57af8a
ChyngyzTorogeldiev <www.araket@gmail.com>
2021-06-06 23:40:47 +0300

    addedsecond test page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
 
 def homepage(request):
     return HttpResponse("hello world!")
 
 def test(request):
     return render(request, "test.html")
+
+
+def second(request):
+    return HttpResponse("test 2 page")
